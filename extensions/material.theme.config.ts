d141e0e5e0f13020184ba700b2080b1c8af59e5b
LasaleFamine <info@godev.space>
2018-05-15 00:15:35 +0200

    chore: added check for autoApplyIcons flag. Removed listen icon change

--- extensions/material.theme.config.ts
+++ extensions/material.theme.config.ts
@@ -1,28 +1,35 @@
 import {
   workspace as Workspace,
-  commands as Commands
+  commands as Commands,
+  ConfigurationChangeEvent
 } from 'vscode';
 
 import * as ThemeCommands from './commands';
 import {shouldShowChangelog, showChangelog} from './helpers/changelog';
 import {reloadWindow, getCurrentThemeID} from './helpers/vscode';
-import {isMaterialTheme} from './helpers/settings';
+import {isMaterialTheme, isAutoApplyEnable} from './helpers/settings';
+
+const onChangeConfiguration = (event: ConfigurationChangeEvent) => {
+  const isColorTheme = event.affectsConfiguration('workbench.colorTheme');
+  const currentTheme = getCurrentThemeID();
+  if (!isMaterialTheme(currentTheme) || !isColorTheme) {
+    return;
+  }
+
+  if (!isAutoApplyEnable()) {
+    return;
+  }
+
+  ThemeCommands.fixIcons()
+    .then(() => reloadWindow())
+    .catch((error: NodeJS.ErrnoException) => console.trace(error));
+};
 
 export function activate() {
   const config = Workspace.getConfiguration();
 
   // Listen on set theme: when the theme is Material Theme, just adjust icon and accent.
-  Workspace.onDidChangeConfiguration(event => {
-    const isColorOrIconTheme = event.affectsConfiguration('workbench.colorTheme') || event.affectsConfiguration('workbench.iconTheme');
-    const currentTheme = getCurrentThemeID();
-    // tslint:disable-next-line:early-exit
-    if (isMaterialTheme(currentTheme) && isColorOrIconTheme) {
-      // TODO: check for `autoFix` flag
-      ThemeCommands.fixIcons()
-        .then(() => reloadWindow())
-        .catch((error: NodeJS.ErrnoException) => console.trace(error));
-    }
-  });
+  Workspace.onDidChangeConfiguration(onChangeConfiguration);
 
   // Delete old configuration, must remove with next major release
   if (config.has('materialTheme.cache.workbench')) {
@@ -34,17 +41,22 @@ export function activate() {
   }
 
   // Registering commands
-  Commands.registerCommand('materialTheme.setAccent', () =>
-    ThemeCommands.accentsSetter()
-      // TODO: check for `autoFix` flag
-      .then(() => ThemeCommands.fixIcons())
+  Commands.registerCommand('materialTheme.setAccent', async () => {
+    await ThemeCommands.accentsSetter();
+
+    if (!isAutoApplyEnable()) {
+      return;
+    }
+
+    ThemeCommands.fixIcons()
       .then(() => reloadWindow())
-      .catch((err: NodeJS.ErrnoException) => console.trace(err))
-  );
+      .catch((err: NodeJS.ErrnoException) => console.trace(err));
+  });
   Commands.registerCommand('materialTheme.fixIcons', () =>
     ThemeCommands.fixIcons()
       .then(() => reloadWindow())
       .catch((err: NodeJS.ErrnoException) => console.trace(err))
   );
+  Commands.registerCommand('materialTheme.toggleApplyIcons', () => ThemeCommands.toggleApplyIcons());
   Commands.registerCommand('materialTheme.showChangelog', () => showChangelog());
 }
