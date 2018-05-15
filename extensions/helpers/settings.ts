7c96f6cad4c685f34ff0e3cb1ebfd38931fd592c
LasaleFamine <info@godev.space>
2018-05-16 00:52:02 +0200

    chore: small change settings method

--- extensions/helpers/settings.ts
+++ extensions/helpers/settings.ts
@@ -51,14 +51,14 @@ export function isMaterialThemeIcons(themeIconsName: string): boolean {
 /**
  * Sets a custom property in custom settings
  */
-export function setCustomSetting(settingName: string, value: any): Thenable<void> {
-  return vscode.workspace.getConfiguration().update(`materialTheme.${settingName}`, value, true);
+export function setCustomSetting(settingName: string, value: any): Thenable<string> {
+  return vscode.workspace.getConfiguration().update(`materialTheme.${settingName}`, value, true).then(() => settingName);
 }
 
 /**
  * Updates accent name
  */
-export function updateAccent(accentName: string): Thenable<void> {
+export function updateAccent(accentName: string): Thenable<string> {
   const prevAccent = getAccent();
   return setCustomSetting('accentPrevious', prevAccent)
     .then(() => setCustomSetting('accent', accentName));
